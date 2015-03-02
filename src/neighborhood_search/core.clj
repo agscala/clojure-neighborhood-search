58d4e23f83ddf82fcecedb466deb3350654d72a4
Andrew Scala <agscala@gmail.com>
2015-03-02 12:35:57 -0500

    Initial commit.

---
+++ src/neighborhood_search/core.clj
@@ -0,0 +1,7 @@
+(ns neighborhood-search.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/neighborhood_search/core_test.clj
@@ -0,0 +1,7 @@
+(ns neighborhood-search.core-test
+  (:require [clojure.test :refer :all]
+            [neighborhood-search.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
