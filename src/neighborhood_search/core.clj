d02d8c31393358f4252722495e8cd9f2edbe0aef
Andrew Scala <agscala@gmail.com>
2015-03-02 13:20:34 -0500

    Requiring yaml and geo libraries.

--- src/neighborhood_search/core.clj
+++ src/neighborhood_search/core.clj
@@ -1,5 +1,8 @@
 (ns neighborhood-search.core
-  (:gen-class))
+  (:gen-class)
+  (:use [geo [poly :as poly]]
+        [clj-yaml.core :as yaml])
+)
 
 (defn -main
   "I don't do a whole lot ... yet."
